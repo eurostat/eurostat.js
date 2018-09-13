76c9d8f70f7a2dbe2fcd73919d2ecebec6913b50
jgaffuri <jgaf@protonmail.com>
2018-09-13 14:56:01 +0200

    a

--- js/eurostat-map.js
+++ js/eurostat-map.js
@@ -7,78 +7,103 @@
  */
 (function(d3, EstLib) {
 
+	//adopt new shema: decompose built
 	//ps: make symbols truly proportionnal
-	//adopt new shema (simple)
 	//add classification method as parameter
 	//add legend
 	//support data flags
 	//check how no-data is handled
 	//insets (with nuts2json)
 	//transform nice nuts map using eurostat-map.js?
+	//Adopt data cache
 
-	EstLib.map = function(svgId, ebcode, dimensions, opts) {
+	EstLib.map = function(opts) {
 		opts = opts || {};
-		opts.type = opts.type || "ch"; //or "ps"
-		opts.width = opts.width || 800;
-		opts.scale = opts.scale || "20M";
-		opts.nutsLvl = opts.nutsLvl || "3";
-		opts.time = opts.time || "2016";
-		opts.proj = opts.proj || "3035";
-		opts.NUTSyear = opts.NUTSyear || 2013;
-		opts.clnb = opts.clnb || 7;
-		opts.lg = opts.lg || "en";
-		opts.preFun = opts.preFun || function() {};
-		opts.postFun = opts.postFun || function() {};
-		opts.zoom = opts.zoom==null? true : opts.zoom;
-		opts.zoomExtentMin = opts.zoomExtentMin || 0.5;
-		opts.zoomExtentMax = opts.zoomExtentMax || 6;
-		opts.drawGraticule = opts.drawGraticule==null? true : opts.drawGraticule;
-		opts.drawCoastalMargin = opts.drawCoastalMargin==null? true : opts.drawCoastalMargin;
-		opts.coastalMarginColor = opts.coastalMarginColor || "white";
-		opts.tooltip = opts.tooltip==null? true : opts.tooltip;
-		opts.unitText = opts.unitText || "";
-		if(opts.type==="ch") opts.classToFillStyle = opts.classToFillStyle || EstLib.getColorLegend(opts.clnb);
-		opts.filtersDefinitionFun = opts.filtersDefinitionFun || function(svg) {};
+		//the id of the svg element to draw into
+		var svgId = opts.svgId || "map";
+		//the width of the svg element, in px
+		var width = opts.width || 800;
+		//the code of the eurobase database
+		var ebcode = opts.ebcode || "demo_r_d3dens";
+		//the dimension projector to extract the statistical data
+		var dimensions = opts.dimensions || { time : opts.time || 2017 };
+		//the map type: "ch" for choropleth and "ps" for proportionnal circles
+		var type = opts.type || "ch"; //or "ps"
+		//the map lod, among 3M, 10M, 20M, 60M
+		var scale = opts.scale || "20M";
+		//the map projection (epsg code)
+		var proj = opts.proj || "3035";
+		//the map nuts level, from 0 to 3
+		var nutsLvl = opts.nutsLvl || "3";
+		//the NUTS version, among 2010, 2013, 2016
+		var NUTSyear = opts.NUTSyear || 2013;
+		//the number of classes of the map
+		var clnb = opts.clnb || 7;
+		//the langage
+		var lg = opts.lg || "en";
+		//if the map is zoomable, specify the scale extent
+		var scaleExtent = opts.scaleExtent || [1,6];
+		//draw the graticule
+		var drawGraticule = opts.drawGraticule==null? true : opts.drawGraticule;
+		//draw the coastal margin
+		var drawCoastalMargin = opts.drawCoastalMargin==null? true : opts.drawCoastalMargin;
+		//the color of the coastal margin
+		var coastalMarginColor = opts.coastalMarginColor || "white";
+		//show tooltip text when passing over map regions
+		var showTooltip = showTooltip==null? true : showTooltip;
+		//the text to use in the tooltip for the unit of the values
+		var unitText = opts.unitText || "";
+		//for choropleth maps, the function returning the fill style depending on the class number (from 0 to clnb-1)
+		var classToFillStyle = opts.classToFillStyle || EstLib.getColorLegend(opts.clnb);
+		//the function defining some fill patterns to be reused for the choropleth map
+		var filtersDefinitionFun = opts.filtersDefinitionFun || function(svg) {};
+		//a function executed after the data has returned
+		var preFun = opts.preFun || function() {};
+		//a function executed at the end of the map building
+		var postFun = opts.postFun || function() {};
 
+		//the output object
+		var out = {};
 
+		out.build = function() {
 
 		d3.queue()
-		.defer(d3.json, "https://raw.githubusercontent.com/eurostat/Nuts2json/gh-pages/" + opts.NUTSyear + "/" + opts.proj + "/" + opts.scale + "/" + opts.nutsLvl + ".json")
+		.defer(d3.json, "https://raw.githubusercontent.com/eurostat/Nuts2json/gh-pages/" + NUTSyear + "/" + proj + "/" + scale + "/" + nutsLvl + ".json")
 		.defer(d3.json, EstLib.getEstatDataURL(ebcode, dimensions))
 		.await(
 				function(error, nuts, data) {
 					//execute prefunction
-					opts.preFun();
+					preFun();
 
 					//tooltip element
-					var tooltip = opts.tooltip? EstLib.tooltip() : null;
+					var tooltip = showTooltip? EstLib.tooltip() : null;
 
 					//decode statistical data
 					data = JSONstat(data).Dataset(0);
 
 					//prepare SVG element
-					var height = opts.width * (nuts.bbox[3] - nuts.bbox[1]) / (nuts.bbox[2] - nuts.bbox[0]),
-						svg = d3.select("#"+svgId).attr("width", opts.width).attr("height", height)
-						path = d3.geoPath().projection(d3.geoIdentity().reflectY(true).fitSize([ opts.width, height ], topojson.feature(nuts, nuts.objects.gra)));
+					var height = width * (nuts.bbox[3] - nuts.bbox[1]) / (nuts.bbox[2] - nuts.bbox[0]),
+						svg = d3.select("#"+svgId).attr("width", width).attr("height", height)
+						path = d3.geoPath().projection(d3.geoIdentity().reflectY(true).fitSize([ width, height ], topojson.feature(nuts, nuts.objects.gra)));
 
-					if(opts.drawCoastalMargin)
+					if(drawCoastalMargin)
 						//define filter for coastal margin
 						svg.append("filter").attr("id", "blur").attr("x","-100%").attr("y", "-100%").attr("width","400%")
 							.attr("height", "400%").append("feGaussianBlur").attr("in", "SourceGraphic").attr("stdDeviation", "4");
 					
 					//add additional filters
-					opts.filtersDefinitionFun(svg);
+					filtersDefinitionFun(svg);
 
 					//draw background rectangle
 					svg.append("rect").attr("id", "sea").attr("x", 0).attr("y", 0)
-							.attr("width", opts.width).attr("height", height)
+							.attr("width", width).attr("height", height)
 							;
 
 					//prepare drawing group
 					var g = svg.append("g").attr("transform", "translate(0,0)");
-					if(opts.zoom) {
+					if(scaleExtent) {
 						//add zoom function
-						svg.call(d3.zoom().scaleExtent([ opts.zoomExtentMin, opts.zoomExtentMax ])
+						svg.call(d3.zoom().scaleExtent(scaleExtent)
 							.on("zoom",
 								function() {
 									var k = d3.event.transform.k;
@@ -91,7 +116,7 @@
 								}));
 					}
 
-					if(opts.drawCoastalMargin) {
+					if(drawCoastalMargin) {
 						//draw coastal margin
 						g.append("g").selectAll("path").data(topojson.feature(nuts, nuts.objects.cntbn).features)
 							.enter().append("path").attr("d", path)
@@ -101,7 +126,7 @@
 							.style("stroke-linejoin", "round")
 							.style("stroke-linecap", "round")
 							.style("stroke", function(bn) {
-									if (bn.properties.co === "T") return opts.coastalMarginColor; return "none";
+									if (bn.properties.co === "T") return coastalMarginColor; return "none";
 							});
 						g.append("g").selectAll("path").data(topojson.feature(nuts, nuts.objects.nutsbn).features)
 							.enter().append("path").attr("d", path)
@@ -111,11 +136,11 @@
 							.style("stroke-linejoin", "round")
 							.style("stroke-linecap", "round")
 							.style("stroke", function(bn) {
-									if (bn.properties.co === "T") return opts.coastalMarginColor; return "none";
+									if (bn.properties.co === "T") return coastalMarginColor; return "none";
 							});
 					}
 
-					if(opts.drawGraticule) {
+					if(drawGraticule) {
 						//draw graticule
 						g.append("g").selectAll("path").data(topojson.feature(nuts,nuts.objects.gra).features)
 							.enter().append("path").attr("d", path)
@@ -128,11 +153,11 @@
 						.enter().append("path").attr("d", path)
 						.attr("class", "cntrg")
 						.on("mouseover",function(rg) {
-							if(opts.tooltip) tooltip.mouseover("<b>" + rg.properties.na + "</b>");
+							if(showTooltip) tooltip.mouseover("<b>" + rg.properties.na + "</b>");
 						}).on("mousemove", function() {
-							if(opts.tooltip) tooltip.mousemove();
+							if(showTooltip) tooltip.mousemove();
 						}).on("mouseout", function() {
-							if(opts.tooltip) tooltip.mouseout();
+							if(showTooltip) tooltip.mouseout();
 						})
 
 					//prepare NUTS regions data
@@ -149,7 +174,7 @@
 					}
 
 					//build list of classes and classification based on quantiles
-					var classif = d3.scaleQuantile().domain(values).range( [...Array(opts.clnb).keys()] );
+					var classif = d3.scaleQuantile().domain(values).range( [...Array(clnb).keys()] );
 					classif.quantiles();
 
 					//draw NUTS regions regions
@@ -160,11 +185,11 @@
 							if (!rg.properties.val) return "nd";
 							return +classif(+rg.properties.val);
 						}).on("mouseover", function(rg) {
-							if(opts.tooltip) tooltip.mouseover("<b>" + rg.properties.na + "</b><br>" + rg.properties.val + (opts.unitText?" "+opts.unitText:""));
+							if(showTooltip) tooltip.mouseover("<b>" + rg.properties.na + "</b><br>" + rg.properties.val + (unitText?" "+unitText:""));
 						}).on("mousemove", function() {
-							if(opts.tooltip) tooltip.mousemove();
+							if(showTooltip) tooltip.mousemove();
 						}).on("mouseout", function() {
-							if(opts.tooltip) tooltip.mouseout();
+							if(showTooltip) tooltip.mouseout();
 						});
 
 					//draw country boundaries
@@ -190,7 +215,7 @@
 						});
 
 
-					if(opts.type == "ps") {
+					if(type == "ps") {
 						//proportionnal symbol map
 						//see https://bl.ocks.org/mbostock/4342045
 						var maxSize = 20;
@@ -207,11 +232,11 @@
 						.enter().append("path").attr("class", "symbol")
 						.attr("d", path.pointRadius(function(d) { return radius(d.properties.val); }))
 						.on("mouseover", function(rg) {
-							if(opts.tooltip) tooltip.mouseover("<b>" + rg.properties.na + "</b><br>" + rg.properties.val + (opts.unitText?" "+opts.unitText:""));
+							if(showTooltip) tooltip.mouseover("<b>" + rg.properties.na + "</b><br>" + rg.properties.val + (unitText?" "+unitText:""));
 						}).on("mousemove", function() {
-							if(opts.tooltip) tooltip.mousemove();
+							if(showTooltip) tooltip.mousemove();
 						}).on("mouseout", function() {
-							if(opts.tooltip) tooltip.mouseout();
+							if(showTooltip) tooltip.mouseout();
 						});
 
 					} else {
@@ -219,16 +244,41 @@
 						//apply style to nuts regions depending on class
 						g.selectAll("path.nutsrg")
 						.attr("fill", function() {
-							return opts.classToFillStyle[ d3.select(this).attr("ecl") ];
+							return classToFillStyle[ d3.select(this).attr("ecl") ];
 						});
 					}
 
 
 					//execute postfunction
-					opts.postFun();
+					postFun();
 
 				});
+		return out;
+		};
 
+		out.svgId = function(v) { if (!arguments.length) return svgId; svgId=v; return out; };
+		out.width = function(v) { if (!arguments.length) return width; width=v; return out; };
+		out.ebcode = function(v) { if (!arguments.length) return ebcode; ebcode=v; return out; };
+		out.dimensions = function(v) { if (!arguments.length) return dimensions; dimensions=v; return out; };		
+		out.type = function(v) { if (!arguments.length) return type; type=v; return out; };
+		out.scale = function(v) { if (!arguments.length) return scale; scale=v; return out; };
+		out.proj = function(v) { if (!arguments.length) return proj; proj=v; return out; };
+		out.nutsLvl = function(v) { if (!arguments.length) return nutsLvl; nutsLvl=v; return out; };
+		out.NUTSyear = function(v) { if (!arguments.length) return NUTSyear; NUTSyear=v; return out; };
+		out.clnb = function(v) { if (!arguments.length) return clnb; clnb=v; return out; };
+		out.lg = function(v) { if (!arguments.length) return lg; lg=v; return out; };
+		out.scaleExtent = function(v) { if (!arguments.length) return scaleExtent; scaleExtent=v; return out; };
+		out.drawGraticule = function(v) { if (!arguments.length) return drawGraticule; drawGraticule=v; return out; };
+		out.drawCoastalMargin = function(v) { if (!arguments.length) return drawCoastalMargin; drawCoastalMargin=v; return out; };
+		out.coastalMarginColor = function(v) { if (!arguments.length) return coastalMarginColor; coastalMarginColor=v; return out; };
+		out.showTooltip = function(v) { if (!arguments.length) return showTooltip; showTooltip=v; return out; };
+		out.unitText = function(v) { if (!arguments.length) return unitText; unitText=v; return out; };
+		out.classToFillStyle = function(v) { if (!arguments.length) return classToFillStyle; classToFillStyle=v; return out; };
+		out.filtersDefinitionFun = function(v) { if (!arguments.length) return filtersDefinitionFun; filtersDefinitionFun=v; return out; };
+		out.preFun = function(v) { if (!arguments.length) return preFun; preFun=v; return out; };
+		out.postFun = function(v) { if (!arguments.length) return postFun; postFun=v; return out; };
+
+		return out;
 	};
 
 
@@ -286,69 +336,6 @@
 
 
 
-
-
-
-	EstLib.map2 = function(){
-		var
-		codesHierarchy = {}, //codesHierarchy: code,children[]
-		div = "sunburst", //the div element where to build the svg element
-		radius = 150 //the chart size
-		;
-
-		//the output object
-		var out = {};
-
-		var
-		shapes
-		;
-
-
-
-		var build = function(){
-			//build
-			//shapes = shapesG.datum(codesHierarchy).selectAll("path")
-			//out.function35325(0);
-		};
-
-		//set data, with transition
-		//values: code:value
-		out.set = function(data, duration){
-			duration = duration || 0;
-
-/*
-			shapes.data(partition.value(function(d) { return values?values[d.code]:1; }).nodes)
-			.transition().duration(duration).attrTween("d", arcTween)
-			.each("end", function(){ out.drawLabels(duration*0.5); })
-			;*/
-			return out;
-		};
-
-
-
-		out.codesHierarchy = function(v) {
-			if (!arguments.length) return codesHierarchy;
-			codesHierarchy=v;
-			build();
-			return out;
-		};
-
-		out.div = function(v) {
-			if (!arguments.length) return div;
-			div=v;
-			build();
-			return out;
-		};
-
-		out.radius = function(v) {
-			if (!arguments.length) return radius;
-			radius=v;
-			build();
-			return out;
-		};
-
-		return out;
-	}
 
 
 }(d3, window.EstLib = window.EstLib || {} ));
