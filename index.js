0a7eb71588e3780926748825c521e3d1afa4bd0e
jgaffuri <jgaf@protonmail.com>
2019-12-03 20:52:15 +0100

    webpack test

---
+++ index.js
@@ -0,0 +1,7 @@
+//See: https://medium.com/the-node-js-collection/modern-javascript-explained-for-dinosaurs-f695e9747b70
+
+var d3 = require("d3");
+
+const test = function(a){
+    console.log(a);
+}
