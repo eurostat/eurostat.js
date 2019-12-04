616cd956eed924aa62911b52740c385420bf3624
jgaffuri <jgaf@protonmail.com>
2019-12-04 21:26:47 +0100

    a

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mode: 'development',
+    entry: './index.js', // entry: ["./lib.js", "./index.js"],
+    output: {
+      filename: 'main.js',
+      publicPath: 'dist'
+    }
+  };
