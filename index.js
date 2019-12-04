2b775ccc1340c9a2a0dbf4c6ed91ccf7de8f9619
jgaffuri <jgaf@protonmail.com>
2019-12-04 21:25:48 +0100

    a

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 //See: https://medium.com/the-node-js-collection/modern-javascript-explained-for-dinosaurs-f695e9747b70
+//See: https://medium.com/@svinkle/getting-started-with-webpack-and-es6-modules-c465d053d988
 
 //var d3 = require("d3");
 
-const test = function(a){
+export var test = function (a) {
     console.log(a);
+    console.log("ahahah!");
 }
+
+test("dfdgdf");
+//var ddd = d3.interpolate;
+console.log(ddd);
