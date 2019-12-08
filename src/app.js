87316f986febe500985a612c76bfbc94b55eb8a3
jgaffuri <jgaf@protonmail.com>
2019-12-08 20:46:26 +0100

    modularisation

---
+++ src/app.js
@@ -0,0 +1,12 @@
+//See: https://medium.com/the-node-js-collection/modern-javascript-explained-for-dinosaurs-f695e9747b70
+//See: https://webpack.js.org/configuration/output/
+//See: https://medium.com/@svinkle/getting-started-with-webpack-and-es6-modules-c465d053d988
+
+
+//var d3 = require("d3");
+//import {sum, product, test} from './eurostat-test.js';
+import "./eurostat-test.js";
+import "./eurostat-lib.js";
+
+//console.log(EstLib.getEstatRestDataURLBase);
+//console.log(EstLib);
---
+++ src/eurostat-test.js
@@ -0,0 +1,12 @@
+export const test = function (a) {
+    console.log(a);
+    console.log("ahahah!");
+}
+
+export const sum = (a, b) => {
+    return a + b;
+};
+
+export const product = (a, b) => {
+    return a * b;
+};
