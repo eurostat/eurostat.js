2ffaebda79ddee1b6fefec2f21805cbfb9af28c9
jgaffuri <jgaf@protonmail.com>
2019-12-10 15:49:02 +0100

    library

--- src/eurostat-test.js
+++ src/eurostat-test.js
@@ -1,11 +1,11 @@
-export const sum = (a, b) => {
+export let sum = (a, b) => {
     return a + b;
 };
 
-export const product = (a, b) => {
+export let product = (a, b) => {
     return a * b;
 };
 
-export const log = (a) => {
+export let log = (a) => {
     console.log(a);
 };
--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@
 //See: https://medium.com/@svinkle/getting-started-with-webpack-and-es6-modules-c465d053d988
 
 
-//var d3 = require("d3");
-//var test = require("./eurostat-test");
-import {sum, product, log} from './eurostat-test';
-//import "./eurostat-lib";
+import * as base from './eurostat-base';
+import * as tooltip from './eurostat-tooltip';
+import * as map from './eurostat-map';
+import * as test from './eurostat-test';
 
-//console.log(EstLib.getEstatRestDataURLBase);
-//console.log(sum(1,1));
-//console.log(product(2,2));
-//log("hi!");
-
-export {sum,product,log}
+export {base, tooltip, map, test}
