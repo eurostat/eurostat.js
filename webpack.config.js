6cb37fc9f159f6b2355d8a1cd0582fdc8c512e99
jgaffuri <jgaf@protonmail.com>
2019-12-28 17:37:01 +0100

    format

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,34 @@
 module.exports = [{
-    entry: './src/index.js',
-    output: {
-      filename: './eurostat-min.js',
-      publicPath: "dist",
-      library: 'EstLib'
-    },
-    //watch: true
-  },
-  {
-    entry: './src/eurostat-base.js',
-    output: {
-      filename: './eurostat-base.js',
-      publicPath: "dist"
-    },
-  },
-  {
-    entry: './src/eurostat-tooltip.js',
-    output: {
-      filename: './eurostat-tooltip.js',
-      publicPath: "dist"
-    },
-  },
-  {
-    entry: './src/eurostat-map.js',
-    output: {
-      filename: './eurostat-map.js',
-      publicPath: "dist"
-    },
-  },
-  {
-    entry: './src/eurostat-map.js',
-    output: {
-      filename: './eurostat-map-min.js',
-      publicPath: "dist",
-      library: 'EstLib'
-    },
+  entry: './src/eurostat-base.js',
+  output: {
+    filename: './eurostat-base.js',
+    publicPath: "dist"
   }
-];
+}, {
+  entry: './src/eurostat-tooltip.js',
+  output: {
+    filename: './eurostat-tooltip.js',
+    publicPath: "dist"
+  }
+}, {
+  entry: './src/eurostat-map.js',
+  output: {
+    filename: './eurostat-map.js',
+    publicPath: "dist"
+  }
+}, {
+  entry: './src/eurostat-map.js',
+  output: {
+    filename: './eurostat-map-min.js',
+    publicPath: "dist",
+    library: 'EstLib'
+  }
+}, {
+  entry: './src/index.js',
+  output: {
+    filename: './eurostat-min.js',
+    publicPath: "dist",
+    library: 'EstLib'
+  }
+  //watch: true
+}];
